6783e9cc4e792ebcbfd970ec97ef6bc84394c3dd
iorid3 <itakeshita@my.bcit.ca>
2023-07-14 12:10:25 -0700

    delete console logs

--- test.ts
+++ test.ts
@@ -1,8 +1,7 @@
-const string  = ["aaaaa"];
+const string  = ["aaaaa123"];
 const makeArray = string[0]
 const newArray = [...makeArray]
 
-console.log(makeArray)
 const firstUniqueElement = newArray.find((el) => newArray.indexOf(el) === newArray.lastIndexOf(el));
 const result = firstUniqueElement !==  undefined ? firstUniqueElement:""
 
